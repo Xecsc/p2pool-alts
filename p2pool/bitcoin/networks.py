07c5aaa572cb4181dd5f56ae94dabf12e1cbe2a9
Xecsc <kc9cjj@gmail.com>
2014-01-16 09:31:00 -0500

    Fixed Franko block explorer

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -673,9 +673,9 @@ nets = dict(
         BLOCK_PERIOD=30, # s targetspacing
         SYMBOL='FRK',
         CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'franko') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/franko/') if platform.system() == 'Darwin' else os.path.expanduser('~/.franko'), 'franko.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://d.evco.in/abe/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://d.evco.in/abe/address/',
-	TX_EXPLORER_URL_PREFIX='http://d.evco.in/abe/tx/',
+        BLOCK_EXPLORER_URL_PREFIX='http://frk.cryptocoinexplorer.com/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://frk.cryptocoinexplorer.com/address/',
+	TX_EXPLORER_URL_PREFIX='http://frk.cryptocoinexplorer.com//tx/',
         SANE_TARGET_RANGE=(2**256//100000000 - 1, 2**256//1000 - 1),
         DUMB_SCRYPT_DIFF=2**16,
 	DUST_THRESHOLD=0.03e8,
