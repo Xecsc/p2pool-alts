f27e9a6730a5a662fdef2f7af0b41b01420b5cf7
Xecsc <kc9cjj@gmail.com>
2014-01-15 19:16:12 -0500

    Add Franko

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -659,6 +659,28 @@ nets = dict(
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0,
     ),
+    franko=math.Object(
+        P2P_PREFIX='7defaced'.decode('hex'),
+        P2P_PORT=7912,
+        ADDRESS_VERSION=35,
+        RPC_PORT=7913,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'frankoaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 1*10000000 >> (height + 1)//1080000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=30, # s targetspacing
+        SYMBOL='FRK',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'franko') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/franko/') if platform.system() == 'Darwin' else os.path.expanduser('~/.franko'), 'franko.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://d.evco.in/abe/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://d.evco.in/abe/address/',
+	TX_EXPLORER_URL_PREFIX='http://d.evco.in/abe/tx/',
+        SANE_TARGET_RANGE=(2**256//100000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+	DUST_THRESHOLD=0.03e8,
+    ),
+
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
